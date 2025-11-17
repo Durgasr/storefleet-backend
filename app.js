022a314e334644de70f853cbb22879bc46eac0ae
Durgasri <challadurgasri2000@gmail.com>
2025-11-17 23:24:02 +0530

    configure routes in app.js file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
+import productRoutes from "./src/product/routes/product.routes";
+import userRoutes from "./src/user/routes/user.routes";
+import orderRoutes from "./src/order/routes/order.routes";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 const configPath = path.resolve("config", "uat.env");
 dotenv.config({ path: configPath });
@@ -10,6 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// configure routes
+app.use("/api/storefleet/product", productRoutes);
+app.use("/api/storefleet/user", userRoutes);
+app.use("/api/storefleet/order", orderRoutes);
 
+// errorHandlerMiddleware
+app.use(errorHandlerMiddleware);
 
 export default app;
